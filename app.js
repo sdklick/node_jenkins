854161acb7dfaa1c2c2a1314ac0d404428481246
sdklick <sumantad827@gmail.com>
2026-03-03 12:37:14 +0530

    add jen

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    const name = req.query.name;
+  res.send(`Docker Compose CI/CD Working by ${name}🚀`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
